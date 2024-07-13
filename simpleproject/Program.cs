95d2f6463c15d964c8da2c3746458e9b9cf19cb8
Siber497 <siber497@gmail.com>
2024-07-13 21:41:05 +0300

    initial commit

---
+++ simpleproject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, учимся github-у");
+        }
+    }
+}
